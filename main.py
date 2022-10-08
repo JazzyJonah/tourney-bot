e12c79c631aa962d478ecf801b6e4660bb124c9f
JazzyJonah <96821467+JazzyJonah@users.noreply.github.com>
2022-10-08 15:30:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ async def on_ready():
 
 @client.slash_command(name="info", description="View info about the bot", guild_ids=testingServersIDs)
 async def help(interaction: Interaction):
-	await interaction.response.send_message("The test command works!")
+	await interaction.response.send_message("""
+		**List of Commands** \n
+		`/info` - What you're seeing right now!
+		`/trailer` - Shows the official trailer for the B2T server
+		`/create_tournament` - Creates a tournament (mod only) (WIP)\n
+		||Created by JazzyJonah (<@627917067332485120>); Source code: https://github.com/JazzyJonah/tourney-bot""")
+
+@client.slash_command(name="trailer", description="The official trailer for B2T", guild_ids=testingServersIDs)
+async def trailer(interaction: Interaction):
+	await interaction.response.send_message("https://www.youtube.com/watch?v=hn5jQF6KRok")
+
 
 client.run(os.getenv("token"))
