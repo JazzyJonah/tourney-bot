2ef889912a23e0c765a40c9007b66b5f00c90599
JazzyJonah <96821467+JazzyJonah@users.noreply.github.com>
2022-10-13 16:56:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from nextcord.abc import GuildChannel
 from nextcord.ext import commands, application_checks
 import nextcord
 import time
+from random import random
 
 from convert import convert
 from createbracket import createbracket
@@ -21,17 +22,24 @@ async def on_ready():
 	await client.change_presence(activity = nextcord.Game(name="Battles 2"))
 
 @client.slash_command(name="info", description="View info about the bot", guild_ids=testingServersIDs)
-async def help(interaction: Interaction):
-	await interaction.response.send_message("""
-		**List of Commands** \n
-		`/info` - What you're seeing right now!
-		`/trailer` - Shows the official trailer for the B2T server
-		`/create_tournament` - Creates a tournament (mod only) (WIP)\n
-		||Created by JazzyJonah (<@627917067332485120>) - Source code: https://github.com/JazzyJonah/tourney-bot||""")
+async def info(interaction: Interaction):
+	if random() < 0.01:
+		await interaction.response.send_message("NO, NOW SHUT UP BOZO")
+	else:
+		await interaction.response.send_message("""
+			**List of Commands** \n
+			`/info` - What you're seeing right now!
+			`/trailer` - Shows the official trailer for the B2T server
+			`/create_tournament` - Creates a tournament (mod only) (WIP)
+			`/bracket` - Sends a bracket of some cool players (WIP)\n
+			||Created by JazzyJonah - Source code: https://github.com/JazzyJonah/tourney-bot||""")
 
 @client.slash_command(name="trailer", description="The official trailer for B2T", guild_ids=testingServersIDs)
 async def trailer(interaction: Interaction):
-	await interaction.response.send_message("https://www.youtube.com/watch?v=hn5jQF6KRok")
+	if random() < 0.01:
+		await interaction.response.send_message("NO, NOW SHUT UP BOZO")
+	else:
+		await interaction.response.send_message("https://www.youtube.com/watch?v=hn5jQF6KRok")
 
 @client.slash_command(name="create_tourney", description="Create a tournament! (Mod only)", guild_ids=testingServersIDs)
 @application_checks.check_any(application_checks.is_owner(), application_checks.has_any_role(929782523146432562, 845011146552508437)) #Tournament Admin, new role
@@ -43,13 +51,16 @@ async def create_tourney(
 		required=True
 	)
 ):
-	try:
-		vrej = convert(date_time.rsplit(" ",1)[0], date_time.split()[-1])
-		await interaction.response.send_message("Tournament starts at <t:" + vrej + "> or <t:" + vrej + ":R>")
-	except Exception as e:
-		await interaction.response.send_message("You messed up somewhere. Try again. (Error message: " + str(e)) + ")"
+	if random() < 0.01:
+		await interaction.response.send_message("NO, NOW SHUT UP BOZO")
+	else:
+		try:
+			vrej = convert(date_time.rsplit(" ",1)[0], date_time.split()[-1])
+			await interaction.response.send_message("Tournament starts at <t:" + vrej + "> or <t:" + vrej + ":R>")
+		except Exception as e:
+			await interaction.response.send_message("You messed up somewhere. Try again. (Error message: " + str(e)) + ")"
 
-	os.remove("bracket.png")
+	# os.remove("bracket.png")
 @create_tourney.error
 async def on_create_tourney_error(interaction: Interaction, error):
   await interaction.response.send_message("You don't have tournament admin.")
@@ -57,10 +68,13 @@ async def on_create_tourney_error(interaction: Interaction, error):
 
 @client.slash_command(name="bracket", description="Sends a bracket of some cool players", guild_ids=testingServersIDs)
 async def bracket(interaction:Interaction):
-	createbracket("Ninjayas", "https://cdn.discordapp.com/avatars/617541280154517641/b38841e95f8cdc0fb8b0374311b7e3e0.png", "JazzyJonah", "https://cdn.discordapp.com/avatars/627917067332485120/195cb543f9006e9024401fe3d6a871cc.png", "B2T", "https://cdn.discordapp.com/avatars/900469656844914729/8e7fe6eebdaf22ff691e477f79fd6ad5.png", "SSAMBO", "https://cdn.discordapp.com/avatars/920358051893104671/b512e4cc93b81134716892b9e08afe5f.png")
-	await interaction.response.defer()
-	await interaction.followup.send(file=nextcord.File("bracket.png"))
-	os.remove("bracket.png")
+	if random() < 0.01:
+		await interaction.response.send_message("NO, NOW SHUT UP BOZO")
+	else:
+		createbracket("Ninjayas", "https://cdn.discordapp.com/avatars/617541280154517641/b38841e95f8cdc0fb8b0374311b7e3e0.png", "JazzyJonah", "https://cdn.discordapp.com/avatars/627917067332485120/195cb543f9006e9024401fe3d6a871cc.png", "B2T", "https://cdn.discordapp.com/avatars/900469656844914729/8e7fe6eebdaf22ff691e477f79fd6ad5.png", "SSAMBO", "https://cdn.discordapp.com/avatars/920358051893104671/b512e4cc93b81134716892b9e08afe5f.png")
+		await interaction.response.defer()
+		await interaction.followup.send(file=nextcord.File("bracket.png"))
+		os.remove("bracket.png")
 
 
 if os.getenv("token")!= None:
